a450ce87ce8e20484c9186db0f7465d067d1e706
CalNHobsGotU <calnhobsgotu@CalNHobsGotUs-MacBook.local>
2022-10-05 21:16:02 -0400

    added a route_table and aws_route connected to it

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "first_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
@@ -24,4 +24,18 @@ resource "aws_internet_gateway" "igw" {
   tags = {
     Name = "dev-igw-1"
   }
+}
+
+resource "aws_route_table" "first_route_table" {
+    vpc_id = aws_vpc.main.id
+
+    tags = {
+        Name = "dev-rt-pub1"
+    }
+}
+
+resource "aws_route" "first_route" {
+  route_table_id            = aws_route_table.first_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
